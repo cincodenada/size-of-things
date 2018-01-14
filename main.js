540fbb3bd3714913adfa46e6bfd755ce38780d6f
Joel Bradshaw <cincodenada@gmail.com>
2018-01-13 23:23:04 -0800

    Go way overboard in converting numbers
    Definitely coulda found a library but I'm having fun

--- main.js
+++ main.js
@@ -3,11 +3,57 @@ var ships;
 var min_size_px = 50
 var m_per_px = 10
 var zoomSlowFactor = 100
-var screen_px_per_mm = 96/2.54;
-var number_names = [
-  'million', 'billion', 'trillion', 'quadrillion','quintillion',
-  'sextillion','septillion','octillion','nonillion','decillion'
-]
+var screen_px_per_mm = 96/2.54
+
+var FancyNumber = (function(number, sigfigs) {
+  var number_names = [
+    'million', 'billion', 'trillion', 'quadrillion','quintillion',
+    'sextillion','septillion','octillion','nonillion','decillion'
+  ]
+  var si_units = ['nm','µm','mm','m','km']
+  var si_offset = 3
+
+  function FancyNumber(number, sigfigs) {
+    if(!sigfigs) { sigfigs = 1 }
+    this.zeroes = Math.floor(Math.log10(number))
+    this.groups = Math.floor(this.zeroes/3)
+    this.number = number
+    this.sigfigs = sigfigs
+  }
+
+  FancyNumber.prototype.setSigFigs = function(digits) {
+    this.sigfigs = digits
+  }
+
+  FancyNumber.prototype.roundSigFig = function() {
+    if(!sigfigs) { sigfigs = 1; }
+    var num_clear = this.zeroes - (sigfigs-1)
+    var out_num = Math.round(this.number/Math.pow(10, num_clear)) * Math.pow(10, num_clear)
+    if(num_clear < 0) { out_num = out_num.toFixed(-num_clear) }
+    return out_num
+  }
+
+  FancyNumber.prototype.getHuman = function() {
+    var out_num = this.roundSigFig()
+    if(this.groups >= 2) {
+      out_num /= Math.pow(10, this.groups*3)
+      out_num = out_num + " " + number_names[this.groups-2]
+    } else if(this.groups == 1) {
+      out_num = out_num.toString().substring(0,this.zeroes-2) + ',' + out_num.toString().substring(this.zeroes-2)
+    }
+    return out_num
+  }
+
+  FancyNumber.prototype.getUnits = function() {
+    var si_idx = this.groups + si_offset;
+    if(si_idx < 0) { si_idx = 0 }
+    if(si_idx >= si_units.length) { si_idx = si_units.length - 1 }
+    return this.roundSigFig()/Math.pow(10, si_idx - si_offset) + " " + si_units[si_idx]
+  }
+
+  return new FancyNumber(number, sigfigs)
+})
+
 $(function() {
   container = $('.stuff')
   $.getJSON("ships.json").done(function(data) {
@@ -53,27 +99,11 @@ function resize() {
       }
     }
   })
-  $('.m_per_px').text(humanize(m_per_px, 3));
+  $('.m_per_px').text((new FancyNumber(m_per_px, 3)).getUnits());
   real_to_screen = m_per_px*1000*screen_px_per_mm
   if(real_to_screen < 1) {
-    $('.ratio').text('1:' + humanize(1/real_to_screen))
+    $('.ratio').text('1:' + new FancyNumber(1/real_to_screen).getHuman())
   } else {
-    $('.ratio').text(humanize(real_to_screen) + ':1')
+    $('.ratio').text(new FancyNumber(real_to_screen).getHuman() + ':1')
   }
-}
-
-function humanize(number, digits) {
-  if(!digits) { digits = 1; }
-  var zeroes = Math.floor(Math.log10(number))
-  var num_clear = zeroes - (digits-1)
-  var groups = Math.floor(zeroes/3)
-  number = Math.round(number/Math.pow(10, num_clear)) * Math.pow(10, num_clear)
-  if(num_clear < 0) { number = number.toFixed(-num_clear) }
-  if(groups >= 2) {
-    number /= Math.pow(10, groups*3)
-    number = number + " " + number_names[groups-2]
-  } else if(groups == 1) {
-    number = number.toString().substring(0,zeroes-2) + ',' + number.toString().substring(zeroes-2)
-  }
-  return number
 }

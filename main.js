277241166594245c9b3efa482a74fc447ba348ac
Joel Bradshaw <cincodenada@gmail.com>
2018-01-13 22:47:51 -0800

    Add zoom scale ratio

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var container;
 var ships;
-var min_size_px = 50;
-var m_per_px = 10;
+var min_size_px = 50
+var m_per_px = 10
 var zoomSlowFactor = 100
+var screen_px_per_mm = 96/2.54;
+var number_names = [
+  'million', 'billion', 'trillion', 'quadrillion','quintillion',
+  'sextillion','septillion','octillion','nonillion','decillion'
+]
 $(function() {
   container = $('.stuff')
   $.getJSON("ships.json").done(function(data) {
@@ -49,4 +54,23 @@ function resize() {
     }
   })
   $('.m_per_px').text(Math.round(m_per_px));
+  real_to_screen = m_per_px*1000*screen_px_per_mm
+  if(real_to_screen < 1) {
+    $('.ratio').text('1:' + humanize(1/real_to_screen))
+  } else {
+    $('.ratio').text(humanize(real_to_screen) + ':1')
+  }
+}
+
+function humanize(number) {
+  var zeroes = Math.floor(Math.log10(number))
+  var groups = Math.floor(zeroes/3)
+  number = Math.round(number/Math.pow(10, zeroes)) * Math.pow(10, zeroes)
+  if(groups >= 2) {
+    number /= Math.pow(10, groups*3)
+    number = number + " " + number_names[groups-2]
+  } else if(groups == 1) {
+    number = Math.floor(number/1e3) + ",000"
+  }
+  return number
 }

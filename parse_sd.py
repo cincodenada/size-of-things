e40473829ec1e64dde7ce72cddd6ca47570af9a9
Joel Bradshaw <joel.bradshaw@cedexis.com>
2018-01-23 18:21:10 -0800

    Improve parsing of 1mpp ships

--- parse_sd.py
+++ parse_sd.py
@@ -1,25 +1,46 @@
+from __future__ import print_function
 from bs4 import BeautifulSoup
 import glob
+import re
+
+def dewhite(desc):
+  desc = re.sub(r"\s+", " ", desc).strip()
+  return desc
 
 for page in glob.glob("Starship Dimensions/1 Pixel*.htm"):
   soup = BeautifulSoup(open(page, 'r'), "lxml")
   category = None
-  for td in soup.body.table.find_all('td'):
+  ships = []
+  for td in soup.body.find_all('td'):
     if td.find('strong'):
-      category = td.find('strong').text
+      category = dewhite(td.find('strong').text)
+      print(category)
     else:
+      images = td.find_all('img')
       lines = td.find_all(['font','img'], recursive=False)
-      if len(lines) == 2:
-        img = lines[0]
-        if img.img:
-          img = img.img
 
-        src = img['src']
-        description = lines[1].text
+      if len(images) == len(lines):
+        # <img><font>Description</font>
+        # <font><img> Description</font>
+        # <td> <font><img></font> x N </td><td> <font>Description</font> </td>
+        for img in images:
+          ship = {}
+          ship['group'] = category
+          ship['src'] = img['src']
+          ship['description'] = dewhite(img.parent.text)
+          if 'alt' in img:
+            ship['name'] = img['alt']
+          ships.append(ship)
       elif len(lines) == 1:
-        src = lines[0].find('img')
-        description = lines[0].text 
+        # <font>Description</font>
+        pass
+      elif len(lines) == len(images)*2:
+        # <font><img></font><font>Description</font>
+        for (idx, img) in enumerate(images):
+          ship = {}
+          ship['group'] = category
+          ship['src'] = img['src']
+          ship['description'] = dewhite(lines[idx*2+1].text)
+          ships.append(ship)
 
-      print category
-      print src
-      print description
+  print("\n".join([str(s) for s in ships]))
